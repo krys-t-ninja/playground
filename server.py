e2dc4fa5d4e904798fdafd77cf5895d85573496b
Krystal <124251248+krys-t-ninja@users.noreply.github.com>
2023-07-03 15:52:48 -0500

    second re-take
    let's see if there is any difference. This assignment reminded me to "DO LESS".

--- server.py
+++ server.py
@@ -2,12 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
+@app.route('/play')
+def rename():
+    return render_template("index.html", num=3, color="blue")
 
+@app.route('/play/<int:num>')
+def second(num):
+    return render_template("index.html", num=num, color="green")
+
+@app.route('/play/<string:color>/<int:num>')
+def third(color, num):
+    return render_template("index.html", color=color, num=num) 
 
 
 
